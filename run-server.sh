0048d8a197e20a475c1006f511db09712d15164a
ant31 <2t.antoine@gmail.com>
2016-10-14 17:19:52 +0200

    Add run-server.sh

---
+++ run-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PORT=${PORT:-5000}
+storage="${STORAGE:-filesystem}"
+echo $storage
+db_class=cnr.models.kv.$storage.db:CnrDB
+blob_class=cnr.models.kv.$storage.blob:Blob
+echo $db_class
+CNR_DB_CLASS=$db_class CNR_BLOB_CLASS=$blob_class gunicorn cnr.api.wsgi:app -b :$PORT --timeout 120 -w 4 --reload
