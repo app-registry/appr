3eacf5c82b20591e0c9b7cbf682f385181dd2a4f
ant31 <2t.antoine@gmail.com>
2016-11-21 12:07:30 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ test_requirements = [
 
 setup(
     name='cnr-server',
-    version='0.2.0',
+    version='0.2.1',
     description="cloud-native app registry server",
     long_description=readme,
     author="Antoine Legrand",
