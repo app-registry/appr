c6b079d25f2e9122308d1f0125bb2985a547fbe7
Antoine Legrand <antoine.legrand@coreos.com>
2017-02-02 20:57:31 +0100

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -5,4 +5,4 @@ echo $storage
 db_class=cnr.models.kv.$storage.db:CnrDB
 blob_class=cnr.models.kv.$storage.blob:Blob
 echo $db_class
-CNR_DB_CLASS=$db_class CNR_BLOB_CLASS=$blob_class gunicorn cnr.api.wsgi:app -b :$PORT --timeout 120 -w 4 --reload
+DATABASE_URL="$HOME/.cnr/packages" CNR_DB_CLASS=$db_class CNR_BLOB_CLASS=$blob_class gunicorn cnr.api.wsgi:app -b :$PORT --timeout 120 -w 4 --reload
