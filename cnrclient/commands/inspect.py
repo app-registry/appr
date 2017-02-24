2b35a0849ea81d1a46d9cb9fa9a901d50010cfff
Antoine Legrand <antoine.legrand@coreos.com>
2017-02-24 19:23:34 +0100

    Fix inspect command

--- cnrclient/commands/inspect.py
+++ cnrclient/commands/inspect.py
@@ -11,6 +11,7 @@ class InspectCmd(CommandBase):
         self.package = options.package
         self.registry_host = options.registry_host
         self.version = options.version
+        self.version_parts = options.version_parts
         self.file = options.file
         self.tree = options.tree
         self.media_type = options.media_type
@@ -23,17 +24,17 @@ class InspectCmd(CommandBase):
         cls._add_mediatype_option(parser)
         cls._add_packagename_option(parser)
         cls._add_packageversion_option(parser)
-        parser.add_argument('--tree', help="List files inside the package", action='store_true', default=False)
+        parser.add_argument('--tree', help="List files inside the package", action='store_true', default=True)
         parser.add_argument('-f', '--file', help="Display a file", default=None)
 
     def _call(self):
         client = self.RegistryClient(self.registry_host)
-        result = client.pull(self.package, version=self.version, media_type=self.media_type)
+        result = client.pull(self.package, version_parts=self.version_parts, media_type=self.media_type)
         package = CnrPackage(result, b64_encoded=False)
-        if self.tree:
-            self.result = "\n".join(package.tree())
-        elif self.file:
+        if self.file:
             self.result = package.file(self.file)
+        elif self.tree:
+            self.result = "\n".join(package.tree())
         else:
             self.result = package.manifest
 
