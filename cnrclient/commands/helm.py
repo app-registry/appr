912d6481d98d3f4c267bd17542e1261a9f36b873
Antoine Legrand <2t.antoine@gmail.com>
2017-05-24 13:11:17 +0200

    Add requirement.yaml overwrite option

--- cnrclient/commands/helm.py
+++ cnrclient/commands/helm.py
@@ -11,7 +11,7 @@ LOCAL_DIR = os.path.dirname(__file__)
 
 class HelmCmd(CommandBase):
     name = 'helm'
-    help_message = "Deploy with Helm on Kubernetes"
+    help_message = 'Deploy with Helm on Kubernetes'
     parse_unknown = True
 
     def __init__(self, options):
@@ -23,43 +23,51 @@ class HelmCmd(CommandBase):
         pull_cmd._call()
         helm_cli = Helm()
         output = helm_cli.action(cmd, pull_cmd.path, helm_opts)
-        self.status = {"result": output}
+        self.status = {'result': output}
         self.render()
 
     @classmethod
     def _install(cls, options, unknown=None):
         cmd = cls(options)
-        cmd._exec_cmd("install", options, unknown)
+        cmd._exec_cmd('install', options, unknown)
 
     @classmethod
     def _upgrade(cls, options, unknown=None):
         cmd = cls(options)
-        cmd._exec_cmd("upgrade", options, unknown)
+        cmd._exec_cmd('upgrade', options, unknown)
 
     @classmethod
     def _dep_pull(cls, options, unknown=None):
+        cmd = cls(options)
         helm_cli = Helm()
-        helm_cli.build_dep()
+        cmd.status = {'result': helm_cli.build_dep(dest=options.dest, overwrite=options.overwrite)}
+        cmd.render()
 
     @classmethod
     def _init_args(cls, subcmd):
         cls._add_registryhost_option(subcmd)
         cls._add_packagename_option(subcmd)
         cls._add_packageversion_option(subcmd)
-        subcmd.add_argument("-t", "--media-type", default="helm",
-                            help=argparse.SUPPRESS)
+        subcmd.add_argument('-t', '--media-type', default='helm', help=argparse.SUPPRESS)
 
-        subcmd.add_argument("--dest", default=tempfile.gettempdir(),
-                            help="directory used to extract resources")
-        subcmd.add_argument("--tarball", action="store_true", default=True,
-                            help=argparse.SUPPRESS)
+        subcmd.add_argument('--dest', default=tempfile.gettempdir(),
+                            help='directory used to extract resources')
+        subcmd.add_argument('--tarball', action='store_true', default=True, help=argparse.SUPPRESS)
+
+    @classmethod
+    def _init_dep_args(cls, subcmd):
+        subcmd.add_argument('--dest', default="appr_charts",
+                            help='directory used to extract resources')
+        subcmd.add_argument('--overwrite', action='store_true', default=False,
+                            help="auto-merge requirements.yaml with the appr dependencies")
 
     @classmethod
     def _add_arguments(cls, parser):
         sub = parser.add_subparsers()
         install_cmd = sub.add_parser('install')
         upgrade_cmd = sub.add_parser('upgrade')
-        dep_pull_cmd = sub.add_parser('dep-pull')
+        dep_pull_cmd = sub.add_parser('dep')
+        cls._init_dep_args(dep_pull_cmd)
         cls._init_args(install_cmd)
         cls._init_args(upgrade_cmd)
         install_cmd.set_defaults(func=cls._install)
--- cnrclient/plugins/helm.py
+++ cnrclient/plugins/helm.py
@@ -1,4 +1,6 @@
+from __future__ import print_function
 import os
+import sys
 import subprocess
 
 import yaml
@@ -7,15 +9,20 @@ from cnrclient.client import CnrClient
 from cnrclient.utils import mkdir_p, parse_package_name
 from cnrclient.pack import CnrPackage
 
-
-__all__ = ['Helm']
+DEFAULT_CHARTS = "appr_charts"
 
 
 def parse_version(version):
-    if version[0] == ":" or version.startswith("channel:"):
-        parts = {'key': 'channel', 'value': version.split(":")[1]}
-    elif version.startswith("sha256:"):
-        parts = {'key': 'digest', 'value': version.split("sha256:")[1]}
+    """
+     Converts a version string to a dict with following rules:
+       if string starts with ':' it is a channel
+       if string starts with 'sha256' it is a digest
+       else it is a release
+    """
+    if version[0] == ':' or version.startswith('channel:'):
+        parts = {'key': 'channel', 'value': version.split(':')[1]}
+    elif version.startswith('sha256:'):
+        parts = {'key': 'digest', 'value': version.split('sha256:')[1]}
     else:
         parts = {'key': 'version', 'value': version}
 
@@ -26,9 +33,13 @@ class Helm(object):
     def __init__(self):
         pass
 
-    def download_appr_deps(self, deps, tarball=False):
-        path = "dep_charts"
-        mkdir_p(path)
+    def download_appr_deps(self, deps, dest=DEFAULT_CHARTS, tarball=False):
+        """
+            Creates a directory 'dep_charts' to download and extract all dependencies
+            fetched from the app-registry server.
+            returns a helm dependency list
+        """
+        mkdir_p(dest)
         helm_deps = {}
         for dep in deps:
             package_parts = parse_package_name(dep['name'])
@@ -36,36 +47,49 @@ class Helm(object):
 
             vparts = parse_version(dep['version'])
             client = CnrClient(package_parts['host'])
-            package_name = "%s/%s" % (package_parts['namespace'], name)
+            package_name = '%s/%s' % (package_parts['namespace'], name)
 
-            pullpack = client.pull_json(package_name, version_parts=vparts, media_type="helm")
+            pullpack = client.pull_json(package_name, version_parts=vparts, media_type='helm')
             package = CnrPackage(pullpack['blob'], b64_encoded=True)
             release = pullpack['release']
-            packagepath = os.path.join(path, package_parts['namespace'])
-            tarpath = "%s-%s.tgz" % (name, release)
-            print "Pulled package: %s (%s) \n -> %s" % (dep['name'], release, packagepath)
+            packagepath = os.path.join(dest, package_parts['namespace'])
+            print('Pulled package: %s (%s) \n -> %s' % (dep['name'], release, packagepath),
+                  file=sys.stderr)
             if tarball:
-                with open(tarpath, 'wb') as tarfile:
+                with open('%s-%s.tgz' % (name, release), 'wb') as tarfile:
                     tarfile.write(package.blob)
             package.extract(packagepath)
 
-            helm_deps[name] = {'name': name,
-                               'version': release,
-                               'repository': 'file://%s/%s' % (packagepath, name)}
+            helm_deps[name] = {
+                'name': name,
+                'version': release,
+                'repository': 'file://%s/%s' % (packagepath, name)
+            }
         return helm_deps
 
-    def build_dep(self):
-        with open("requirements.yaml", "rb") as requirefile:
+    def build_dep(self, dest=DEFAULT_CHARTS, overwrite=False):
+        """
+            Reads the dependencies from the requirements.yaml, downloads the packages and updates
+            the requirements.yaml.
+            Returns status
+        """
+        with open('requirements.yaml', 'rb') as requirefile:
             deps = yaml.safe_load(requirefile.read())
         helm_deps = {}
-        if "appr" in deps:
-            helm_deps = self.download_appr_deps(deps['appr'])
+        if 'appr' in deps and deps['appr']:
+            helm_deps = self.download_appr_deps(deps['appr'], dest)
             dict_deps = {dep['name']: dep for dep in deps['dependencies']}
             dict_deps.update(helm_deps)
             deps['dependencies'] = dict_deps.values()
-            with open('requirements.yaml', "wb") as requirefile:
-                requirefile.write(yaml.safe_dump(deps, default_flow_style=False))
-            print "Updated requirements.yaml"
+            requirement_output = yaml.safe_dump(deps, default_flow_style=False)
+            if overwrite:
+                with open('requirements.yaml', 'wb') as requirefile:
+                    requirefile.write(requirement_output)
+                return 'Updated requirements.yaml'
+            else:
+                return requirement_output
+        else:
+            return "No appr-registries dependencies"
 
     def action(self, cmd, release, helm_opts=None):
         cmd = [cmd]
@@ -78,5 +102,5 @@ class Helm(object):
         command = ['helm'] + cmd
         try:
             return subprocess.check_output(command, stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            return e.output
+        except subprocess.CalledProcessError as exc:
+            return exc.output
