c3f4cceb0c2971d527051d0adb5ccfb060826ab8
Antoine Legrand <2t.antoine@gmail.com>
2017-07-21 16:58:40 +0200

    Rc-release

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ requirements = base_requirements + cli_requirements + server_requirements + extr
 
 setup(
     name='appr',
-    version='0.6.2',
+    version='0.7.0-pre',
     description="cloud-native app registry server",
     long_description=readme,
     author="Antoine Legrand",
